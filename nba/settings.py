5acb7d6cece40ba137dd19cd9d4d4fd257c0078a
Alex <you@example.com>
2022-11-01 21:06:32 -0400

    changed db settings

--- nba/settings.py
+++ nba/settings.py
@@ -91,12 +91,21 @@ WSGI_APPLICATION = 'nba.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # if DEVELOPMENT_MODE is True:
+# DATABASES = {
+# 	"default": {
+# 		"ENGINE": "django.db.backends.sqlite3",
+# 		"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+# 	}
+# }
+
 DATABASES = {
-	"default": {
-		"ENGINE": "django.db.backends.sqlite3",
-		"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-	}
+    'default': dj_database_url.config(
+        default="sqlite:///{}".format(
+            os.path.join(BASE_DIR, 'db/db.sqlite3')
+        )
+    )
 }
+
 # elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 #     if os.getenv("DATABASE_URL", None) is None:
 #         raise Exception("DATABASE_URL environment variable not defined")
