93565ee6929c2acc6a547c48e0ece87ede6223ce
Alex <you@example.com>
2022-11-01 20:57:37 -0400

    no locals

--- nba/settings.py
+++ nba/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
